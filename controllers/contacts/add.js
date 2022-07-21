5ffa1fed1e57fff33cc58c829df6626a7611f119
tsimbal_ivan <ivan.t@coreb.in>
2022-07-21 21:49:01 +0300

    Initial Commit

---
+++ controllers/contacts/add.js
@@ -0,0 +1,9 @@
+import Contact from "../../models/contacts.js";
+
+const getById = async (req, res) => {
+  const result = await Contact.create(req.body);
+
+  res.status(201).json(result);
+};
+
+export default getById;
